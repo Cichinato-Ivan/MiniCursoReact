1b9bd204935b889b4b382f269beb8af773880bbd
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2021-08-19 20:21:36 +0100

    config button e estilização button

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import Button from './Button';
+
+import './AddTask.css';
+
 const AddTask = () => {
-    return ( <input type="text" /> );
+    return ( 
+        <div className="add-task-container">
+            <input className="add-task-input" type="text"/> 
+            <div className="add-task-button-container">
+                <Button>Adicionar</Button>
+            </div>
+        </div>
+    );
 }
  
 export default AddTask;
